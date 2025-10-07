513481cb71d23ff2ac82139131d5575e4fac7072
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2025-10-07 08:47:13 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,25 +1,10 @@
 # You can remove 'pass' if you written code in the function 
 # Exercise 1
-def max_min(lst):
-    # Write your code here
+def grade_report(input_file, output_file):
     pass
 
 # Exercise 2
-def list_with_reverse(lst):
-    # Write your code here
+def calculate_balance(input_file, output_file):
     pass
 
-# Exercise 3
-def count_target(lst, target):
-    # Write your code here
-    pass
 
-# Exercise 4
-def index_of_max(lst):
-    # Write your code here
-    pass
-
-# Exercise 5
-def squares_list(n):
-    # Write your code here
-    pass
